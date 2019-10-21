243ba0e68e190681abe31df6f72a95f64175716d
Hemanth Bollamreddi <blmhemu@gmail.com>
2019-10-21 22:20:30 +0530

    Corrected the order of rmmmod

--- nvidia-off.sh
+++ nvidia-off.sh
@@ -2,14 +2,14 @@ if optimus-manager --print-mode | grep -iw intel ; then
     if modinfo acpi_call &> /dev/null ; then
         modprobe acpi_call
     fi
-    if modinfo nvidia &> /dev/null; then
-        rmmod nvidia
-    fi
     if modinfo nvidia_drm &> /dev/null; then
         rmmod nvidia_drm
     fi
     if modinfo nvidia_modeset &> /dev/null; then
         rmmod nvidia_modeset
+    fi
+    if modinfo nvidia &> /dev/null; then
+        rmmod nvidia
     fi
     tee /sys/bus/pci/devices/0000:01:00.0/remove <<< 1
     tee /proc/acpi/call <<< '\_SB.PCI0.PEG0.PEGP._OFF'
