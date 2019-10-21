aa8c23388d0c23e906b3ddb2a0da015c6e7fa7fb
Hemanth Bollamreddi <blmhemu@gmail.com>
2019-10-21 22:42:11 +0530

    Fast safe-polybar-launch in nvidia mode.

--- safe-polybar-launch.sh
+++ safe-polybar-launch.sh
@@ -1,5 +1,10 @@
-sudo /home/blmhemu/nvidia-scripts/nvidia-on.sh
-al-polybar-session
-sleep 5
-while ! pgrep -x polybar > /dev/null; do sleep 1 ;done
-sudo /home/blmhemu/nvidia-scripts/nvidia-off.sh
+if optimus-manager --print-mode | grep -iw intel;
+then
+    sudo /home/blmhemu/nvidia-scripts/nvidia-on.sh
+    al-polybar-session
+    sleep 5
+    while ! pgrep -x polybar > /dev/null; do sleep 1 ;done
+    sudo /home/blmhemu/nvidia-scripts/nvidia-off.sh
+else
+    al-polybar-session
+fi
